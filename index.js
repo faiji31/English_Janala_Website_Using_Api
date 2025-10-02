84dfd61f55e0eef06873c8cb1404dea2dc5b73ca
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-10-02 19:55:47 +0600

    10th commit

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
-
-
-const loadlessons=()=>{
-    fetch("https://openapi.programming-hero.com/api/levels/all")  
-    // promise of response
+const loadlesson=()=>{
+    fetch("https://openapi.programming-hero.com/api/levels/all")
     .then(res=>res.json())
-    .then((json)=>displayLesson(json.data))
+    .then(json=>displaylesson(json.data))
+}
 
-};
-
-const displayLesson =(lessons)=>{
-    const levelContainer = document.getElementById('level-container')
-    levelContainer.innerHTML ="";
-
+displaylesson=(lessons)=>{
+    const displayshow = document.getElementById('level-container')
+    displayshow.innerHTML="";
     for(let lesson of lessons){
         const btndiv = document.createElement('div')
-        btndiv.innerHTML =`
+        btndiv.innerHTML=`
         
-<button  class="btn btn-outline btn-primary "><i class="fa-solid fa-book-open"></i> lesson - ${lesson.level_no} </button>
+        <button  class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson-${lesson.level_no}</button>
 
-
+        
+        
+        
         
         `;
-        levelContainer.append(btndiv);
-
+        displayshow.append(btndiv);
     }
-
-};
-loadlessons();
+}
+loadlesson();
