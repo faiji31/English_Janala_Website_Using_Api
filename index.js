4eaaaa8552d48d9f9f75f2fd215f3bb10fdc2b6b
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-10-02 15:28:18 +0600

    9th commit

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-const { createElement } = require("react");
+
 
 const loadlessons=()=>{
     fetch("https://openapi.programming-hero.com/api/levels/all")  
     // promise of response
     .then(res=>res.json())
     .then((json)=>displayLesson(json.data))
+
 };
 
-const displayLesson =(lesson)=>{
-    const levelContainer = document.getElementById('level-conatiner')
-    levelContainer.innerHTML =" ";
+const displayLesson =(lessons)=>{
+    const levelContainer = document.getElementById('level-container')
+    levelContainer.innerHTML ="";
 
     for(let lesson of lessons){
         const btndiv = document.createElement('div')
         btndiv.innerHTML =`
         
+<button  class="btn btn-outline btn-primary "><i class="fa-solid fa-book-open"></i> lesson - ${lesson.level_no} </button>
+
+
         
-        `
+        `;
+        levelContainer.append(btndiv);
 
     }
 
-}
+};
+loadlessons();
